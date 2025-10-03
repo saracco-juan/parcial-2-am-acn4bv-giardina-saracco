5ea7397cee960515175256aed0a31fe425220b66
Mariano <marianogiardinamonti1@gmail.com>
2025-10-03 16:14:14 -0300

    Feature: background finalizado, estilos desde archivo drawable

--- app/src/main/java/com/example/glypha_primer_parcial_giardina_saracco/MainActivity.java
+++ app/src/main/java/com/example/glypha_primer_parcial_giardina_saracco/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Log.d("test-app", "Corre el emulador");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
