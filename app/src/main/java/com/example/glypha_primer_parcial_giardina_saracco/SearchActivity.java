250c323c71ccec461d99e53bf1d04c30d584d1f9
juan-saracco <juansaracco1212@gmail.com>
2025-11-21 15:00:36 -0300

    Feature: Search Activity added.

---
+++ app/src/main/java/com/example/glypha_primer_parcial_giardina_saracco/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.glypha_primer_parcial_giardina_saracco;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_activity);
+    }
+}
